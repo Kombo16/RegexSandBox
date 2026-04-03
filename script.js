b2b3b8c95e35bf5e38ff46529da8c0130ebac9c5
Kombo16 <134960568+Kombo16@users.noreply.github.com>
2026-04-03 17:46:35 +0300

    Created a function that returns flags checked

--- script.js
+++ script.js
@@ -10,18 +10,24 @@ const testButton = document.getElementById("test-btn");
 const testResult = document.getElementById("result");
 const caseInsensitiveFlag = document.getElementById("i");
 const globalFlag = document.getElementById("g");
+const flags = document.getElementsByName('flags');
+const flagsContainer = document.getElementById("flags-container");
 /*Functions */
-
-/*1. Function that builds the regex
-  input: value of pattern input variable
-  output: A full regular expression
-*/
 
 /*2. Function that updates the regex based on the flag checked by the checkbox
   input: checkbox vaiable
   description: confirms which checkbox has been checked and returns its value
   output: value of checkbox if checked, empty if nothing is checked
 */
+function getFlags(){
+    if (caseInsensitiveFlag.checked && globalFlag.checked){
+        return "gi"
+    }
+    else if (caseInsensitiveFlag.checked || globalFlag.checked){
+        return (caseInsensitiveFlag.checked) ? "i" : "g";
+    }
+    else return "";
+}
 
 /*3. Function that test the input against the built regex
 and prints output
@@ -34,3 +40,6 @@ and prints output
 //Button click listener to call function 3
 //Checkbox change listener to update function 2's return value
 //regex change listener that updates function 1's return value
+flagsContainer.addEventListener("change",()=>{
+    console.log(getFlags());
+})
