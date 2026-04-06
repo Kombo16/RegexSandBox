fa20838f193b17124450a93a2031c1520cc85f70
Kombo16 <134960568+Kombo16@users.noreply.github.com>
2026-04-06 13:42:27 +0300

    Added function that tests the string and an event listener for test button

--- script.js
+++ script.js
@@ -11,15 +11,14 @@ const testResult = document.getElementById("result");
 const caseInsensitiveFlag = document.getElementById("i");
 const globalFlag = document.getElementById("g");
 const flagsContainer = document.getElementById("flags-container");
+//Variable that stores the regular expression
+let regExp;
 /*Functions */
 
 /*1. Function that assembles the full regex
     input: user regex input, flags checked
     output: full regex pattern
 */
-function fullRegexValue(input, flags) {
-    return `/${input}/${flags}`;
-}
 /*2. Function that updates the regex based on the flag checked by the checkbox
   input: checkbox variable
   description: confirms which checkbox has been checked and returns its value
@@ -27,12 +26,12 @@ function fullRegexValue(input, flags) {
 */
 function getFlags(){
     if (caseInsensitiveFlag.checked && globalFlag.checked){
-        return "gi"
+        return "gi";
     }
     else if (caseInsensitiveFlag.checked || globalFlag.checked){
         return (caseInsensitiveFlag.checked) ? "i" : "g";
     }
-    else return "";
+    return "";
 }
 /*3. Function that test the input against the built regex
 and prints output
@@ -40,10 +39,28 @@ and prints output
   description: Takes string and regex pattern and checks for all occurences of the regex in string
   output: returns values gotten to result.textContent else return no match
 */
-
+function testString(string, regex){
+    if (regex.test(string) && globalFlag.checked){
+        return string.matchAll(regExp)[0];
+    }
+    else if (!globalFlag.checked){
+        return string.match(regExp)[0];
+    }
+    return "No matches"
+}
 /*Event Listeners */
 //Button click listener to call function 3
 //Checkbox change listener to update function 2's return value
 regexPattern.addEventListener("input",()=>{
-    console.log(fullRegexValue(regexPattern.value, getFlags()));
+    regExp = RegExp(regexPattern.value, getFlags());
+    console.log(regExp);
+})
+testButton.addEventListener("click", ()=>{
+    if (stringToTest.textContent == "") {
+        testResult.textContent = "Nothing to test";
+    }
+    else {
+        //RegExp(string, flags)
+        testResult.textContent = testString(stringToTest.textContent, regExp)
+    }
 })
